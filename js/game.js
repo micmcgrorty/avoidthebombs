0b0b0ed73f0685da46f3c91442690cd67fc8ad41
micmcgrorty <37194621+micmcgrorty@users.noreply.github.com>
2018-11-18 17:54:48 +0000

    Added basic high score tracking

--- js/game.js
+++ js/game.js
@@ -1,7 +1,6 @@
 let mainScene = new Phaser.Scene('main');
 let titleScene = new Phaser.Scene('title');
 
-
 let config = {
         type: Phaser.AUTO,
         width: 800,
@@ -18,9 +17,9 @@ let config = {
     let cursors;
     let bombs;
     let gameOver = false;
-    let playing = false;
     let score = 0;
     let engineSound;
+    let highScore = 0;
 
     let game = new Phaser.Game(config);
 
@@ -28,6 +27,7 @@ let config = {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('bomb', 'assets/bomb.png');
         this.load.image('ship', 'assets/shuttle2.png');
+        this.load.image('laser', 'assets/star.png');
 
         this.load.audio('engine', 'assets/engine.wav');
         this.load.audio('gameover', 'assets/gameover.wav');
@@ -35,7 +35,9 @@ let config = {
 
     mainScene.create = function() {
         this.add.image(400, 300, 'sky');
-        scoreText = this.add.text(16, 16, 'Score: ', {fontSize: '16px'});
+        scoreText = this.add.text(16, 16, 'Score: 0', {fontSize: '16px'});
+        highScoreText = this.add.text(660, 16, 'High Score: ' + highScore, {fontSize: '16px'});
+        
         player = this.physics.add.sprite(400, 300, 'ship').setScale(0.7);
         player.setBounce(1);
 
@@ -56,9 +58,15 @@ let config = {
         gameOver = false;
     
         score = 0;
+
     }
 
     mainScene.update = function() {
+
+        if (score > highScore) {
+            highScore = score;
+        }
+
         if (cursors.space.isDown && gameOver) {
             this.scene.start('title');
         }
@@ -68,6 +76,7 @@ let config = {
         if (gameOver) {
             this.add.text(200, 150, "GAME OVER!", { fontSize: '64px' });
             this.add.text(100, 300, "PRESS SPACE BAR TO RESTART!", { fontSize: '32px'});
+            highScoreText.setText('High Score: ' + highScore);
 
             return;
         }
@@ -99,7 +108,7 @@ let config = {
         } else if (player.y > 610) {
             player.y = 0;
         }
-        
+
     }
 
     function collideWithBomb(player, bomb) {
@@ -110,6 +119,7 @@ let config = {
         gameOver = true;
 
         this.sound.play('gameover');
+
     }
 
     function addBomb() {
